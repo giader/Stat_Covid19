e3ecda1f9daa0da83c1b02997165c6986506664c
Gianni De Rossi <giaderblog@gmail.com>
2020-04-01 01:31:26 +0200

    Add files via upload

--- source_r/covid19-ita-jmr.R
+++ source_r/covid19-ita-jmr.R
@@ -2,10 +2,9 @@
 setwd("C:/Users/Gianni/Dropbox/R_JSON")
 load("./R_json.RData")
 save.image("./R_json.RData") 
-# Load the package required to read JSON files.
-library(rjson)
-library(dplyr)
-library(jsonlite)
+# Using the logical operator &, it's possible to load all three packages at once and store the result in a single variable.
+lcheck <- require(rjson) & require(dplyr) & require(jsonlite) & require(ggplot2)
+
 # Give the input file name to the function.
 result_regioni <- fromJSON("./Data/dpc-covid19-ita-regioni.json", flatten=TRUE)
 result_nazione <- fromJSON("./Data/dpc-covid19-ita-andamento-nazionale.json", flatten=TRUE)
@@ -30,10 +29,11 @@ class(covid19_it)
 covid19_it$denominazione_regione<-as.factor(covid19_it$denominazione_regione)
 covid19_it <- covid19_it %>% select(everything(), -lat, -long)
 
-names(covid19_it_sum)
+names(covid19_it); 
+names(covid19_it_sum);
 # new Growth rate columns
-covid19_it_sum$Perc_nuovi_attualmente_positivi <- (covid19_it_sum$nuovi_attualmente_positivi / 
-                                                     lag(covid19_it_sum$nuovi_attualmente_positivi, k=1)-1)
+covid19_it_sum$Perc_var_tot_positivi <- (covid19_it_sum$variazione_totale_positivi / 
+                                                     lag(covid19_it_sum$variazione_totale_positivi, k=1)-1)
 
 # Growth rate function
 Perc_covid19 <- function(x, n) {
@@ -41,9 +41,9 @@ Perc_covid19 <- function(x, n) {
 }
 
 ts_n = 1  ### Lag
-covid19_it_sum$Perc_nuovi_attualmente_positivi = Perc_covid19(covid19_it_sum$nuovi_attualmente_positivi, ts_n)
+covid19_it_sum$Perc_var_tot_positivi = Perc_covid19(covid19_it_sum$variazione_totale_positivi, ts_n)
 covid19_it_sum$Perc_totale_ospedalizzati = Perc_covid19(covid19_it_sum$totale_ospedalizzati, ts_n)
-covid19_it_sum$Perc_tot_attualmente_positivi = Perc_covid19(covid19_it_sum$totale_attualmente_positivi, ts_n)
+covid19_it_sum$Perc_nuovi_positivi = Perc_covid19(covid19_it_sum$nuovi_positivi, ts_n)
 covid19_it_sum$Perc_terapia_intensiva = Perc_covid19(covid19_it_sum$terapia_intensiva, ts_n)
 covid19_it_sum$Perc_deceduti = Perc_covid19(covid19_it_sum$deceduti, ts_n)
 covid19_it_sum$Perc_tot_casi = Perc_covid19(covid19_it_sum$totale_casi, ts_n)
@@ -97,31 +97,32 @@ covid19_it_regions <- covid19_it %>%
   select(everything())  
 
 covid19_Lomb<-covid19_it_regions[covid19_it_regions$codice_regione == 3,  ]
-covid19_Tosc<-covid19_it_regions[covid19_it_regions$codice_regione == 9 & covid19_it_regions$data >= "2020-02-28",  ]
+##covid19_Tosc<-covid19_it_regions[covid19_it_regions$codice_regione == 9 & covid19_it_regions$data >= "2020-02-28",  ]
+covid19_Tosc<-covid19_it_regions[covid19_it_regions$codice_regione == 9,  ]
 covid19_Abbr<-covid19_it_regions[covid19_it_regions$codice_regione ==13,  ]
 
 
 
 ts_n = 1  ### Lag
-covid19_Lomb$Perc_nuovi_attualmente_positivi = Perc_covid19(covid19_Lomb$nuovi_attualmente_positivi, ts_n)
+covid19_Lomb$Perc_var_tot_positivi = Perc_covid19(covid19_Lomb$variazione_totale_positivi, ts_n)
 covid19_Lomb$Perc_totale_ospedalizzati = Perc_covid19(covid19_Lomb$totale_ospedalizzati, ts_n)
-covid19_Lomb$Perc_tot_attualmente_positivi = Perc_covid19(covid19_Lomb$totale_attualmente_positivi, ts_n)
+covid19_Lomb$Perc_nuovi_positivi = Perc_covid19(covid19_Lomb$nuovi_positivi, ts_n)
 covid19_Lomb$Perc_ricov_con_sintomi = Perc_covid19(covid19_Lomb$ricoverati_con_sintomi, ts_n)
 covid19_Lomb$Perc_terapia_intensiva = Perc_covid19(covid19_Lomb$terapia_intensiva, ts_n)
 covid19_Lomb$Perc_deceduti = Perc_covid19(covid19_Lomb$deceduti, ts_n)
 covid19_Lomb$Perc_tot_casi = Perc_covid19(covid19_Lomb$totale_casi, ts_n)
 covid19_Lomb$Perc_tamponi = Perc_covid19(covid19_Lomb$tamponi, ts_n)
 
-covid19_Tosc$Perc_nuovi_attualmente_positivi = Perc_covid19(covid19_Tosc$nuovi_attualmente_positivi, ts_n)
+covid19_Tosc$Perc_var_tot_positivi = Perc_covid19(covid19_Tosc$variazione_totale_positivi, ts_n)
 covid19_Tosc$Perc_totale_ospedalizzati = Perc_covid19(covid19_Tosc$totale_ospedalizzati, ts_n)
-covid19_Tosc$Perc_tot_attualmente_positivi = Perc_covid19(covid19_Tosc$totale_attualmente_positivi, ts_n)
+covid19_Tosc$Perc_nuovi_positivi = Perc_covid19(covid19_Tosc$nuovi_positivi, ts_n)
 covid19_Tosc$Perc_ricov_con_sintomi = Perc_covid19(covid19_Tosc$ricoverati_con_sintomi, ts_n)
 covid19_Tosc$Perc_terapia_intensiva = Perc_covid19(covid19_Tosc$terapia_intensiva, ts_n)
 covid19_Tosc$Perc_deceduti = Perc_covid19(covid19_Tosc$deceduti, ts_n)
 covid19_Tosc$Perc_tot_casi = Perc_covid19(covid19_Tosc$totale_casi, ts_n)
 covid19_Tosc$Perc_tamponi = Perc_covid19(covid19_Tosc$tamponi, ts_n)
 
-library(ggplot2)
+#require(ggplot2)
 png("./images/Covid19_it_3reg.png", 500,500)
 covid19_it_regions %>% bind_rows(covid19_it_regions %>% 
                            group_by(data, denominazione_regione) %>% 
@@ -146,7 +147,7 @@ covid19_it_sum %>%
 # save plot images
 png("./images/Covid19_it_cum.png", 500,500)
 covid19_it_sum %>%
-  tidyr::gather(key,value,nuovi_attualmente_positivi, totale_attualmente_positivi, totale_casi, deceduti) %>%
+  tidyr::gather(key,value,variazione_totale_positivi, nuovi_positivi, totale_casi, deceduti) %>%
   ggplot(aes(x=data, y=value, colour=key)) +
   geom_line(size= 1.5) + 
   labs(y = "Total Covid-19 cases", title = "Covid19 - Italy", 
@@ -155,9 +156,19 @@ dev.off()
 
 png("./images/Covid19_it_newcases.png", 500,500)
 covid19_it_sum %>%
-  tidyr::gather(key,value,nuovi_attualmente_positivi) %>%
+  tidyr::gather(key,value,variazione_totale_positivi, nuovi_positivi ) %>%
   ggplot(aes(x=data, y=value, colour=key)) +
   geom_line(size= 1.5) + 
+  labs(y = "Daily new incident confirmed Covid-19 cases", title = "Covid19 - Italy", 
+       subtitle = "(Note: total units)", caption = "giader >>> Source: https://github.com/pcm-dpc/COVID-19") 
+dev.off()
+
+png("./images/Covid19_it_newcasesSmoothed.png", 500,500)
+covid19_it_sum %>%
+  tidyr::gather(key,value, nuovi_positivi ) %>%
+  ggplot(aes(x=data, y=value, colour=key)) +
+  geom_line(size= 1.5) + 
+  geom_smooth(method="auto", se=TRUE, col="steelblue")
   labs(y = "Daily new incident confirmed Covid-19 cases", title = "Covid19 - Italy", 
        subtitle = "(Note: total units)", caption = "giader >>> Source: https://github.com/pcm-dpc/COVID-19") 
 dev.off()
@@ -173,7 +184,7 @@ dev.off()
 
 png("./images/Covid19_it_Perc.png", 500,500)
 covid19_it_sum %>%
-  tidyr::gather(key,value,Perc_nuovi_attualmente_positivi, Perc_tot_attualmente_positivi, Perc_tot_casi) %>%
+  tidyr::gather(key,value,Perc_var_tot_positivi, Perc_nuovi_positivi, Perc_tot_casi) %>%
   ggplot(aes(x=data, y=value, colour=key)) +
   geom_line(size= 1.5) +
   labs(y = "Growth rate (%) d/d", x = "date", title = "Covid19 - Italy", 
@@ -183,7 +194,7 @@ dev.off()
 
 png("./images/Covid19_it_Lombardia.png", 500,500)
 covid19_Lomb %>%
-  tidyr::gather(key,value,Perc_ricov_con_sintomi, Perc_tot_attualmente_positivi, Perc_tot_casi) %>%
+  tidyr::gather(key,value,Perc_nuovi_positivi, Perc_tot_casi) %>%
   ggplot(aes(x=data, y=value, colour=key)) +
   geom_line(size= 1.5) +
   labs(y = "Growth rate (%) d/d", x = "date", title = "Covid19 - Lombardia, Italy", 
@@ -193,7 +204,7 @@ dev.off()
 
 png("./images/Covid19_it_Toscana.png", 500,500)
 covid19_Tosc %>%
-  tidyr::gather(key,value,Perc_ricov_con_sintomi, Perc_tot_attualmente_positivi, Perc_tot_casi) %>%
+  tidyr::gather(key,value,Perc_nuovi_positivi, Perc_tot_casi) %>%
   ggplot(aes(x=data, y=value, colour=key)) +
   geom_line(size= 1.5) +
   labs(y = "Growth rate (%) d/d", x = "date", title = "Covid19 - Tuscany, Italy", 
@@ -225,8 +236,25 @@ covid19_it_sum %>%
        caption = "giader >>>  Source: https://github.com/pcm-dpc/COVID-19") +
   bestfit
 
+par(mfrow=c(1,1))
+###   TS analysis test
+#FastFourierTrasform Periodogram 
+require(TSA)
+periodogram(covid19_it_sum$totale_casi)   
+p<-periodogram(covid19_it_sum$totale_casi)
+
+library(forecast)
+
+#Esempio funzione ts package {stats}
+tsCovid19_it = ts(rev(covid19_it_sum$totale_casi),start=c(2020, 2),end=c(2020,3),frequency=mean(top2[,1]))
+Acf(tsCovid19_it)
+Pacf(tsCovid19_it)
+(tsCovid19_it2 <- round(acf(tsCovid19_it, 6, plot=F)$acf[-1], 3))
 
 
+tseries<-covid19_it_sum$totale_casi %>% msts( seasonal.periods = c(1, 1*7))
+#Plot 
+tseries  %>% head(  24 *7 *4 ) %>% mstl() %>% autoplot() 
 
 
 
